94c5917c0fe72b2daee906e957273745a7406e4f
aurelcode <aurelien.guillou@edu.univ-fcomte.fr>
2023-03-22 15:28:59 +0100

    test absence de jeton + contenu public

--- test/testUser.js
+++ test/testUser.js
@@ -14,4 +14,27 @@ describe("arriver sur la page d'accueil", function(){
             })
         })
     })
+});
+
+describe("recuperation contenu user", function(){
+    describe("contenu pour tous", function(){
+        var url = "http://localhost:3000/test/all";
+        it("retourne contenu public", function(done){
+            request(url, function(error, response, body){
+                expect(response.statusCode).to.equal(200);
+                expect(response.body).to.be.a("string");
+                expect(response.body).to.equal("Contenu public.");
+                done();
+            })
+        })
+    })
+    describe("contenu user", function(){
+        var url = "http://localhost:3000/test/user";
+        it("bloque car pas de jeton", function(done){
+            request(url, function(error, response, body){
+                expect(response.statusCode).to.equal(403);
+                done();
+            })
+        })
+    })
 })
