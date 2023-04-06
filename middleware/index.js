01298f960d192837ae3ceb9265454f26813ea240
GitGudShu <shuukuro@outlook.fr>
2023-04-06 18:26:10 +0200

    Update authRole in middleware/index.js

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,8 @@
 const authJwt = require("./authJwt");
 const verifySignUp = require("./verifySignUp");
+const authRole = require("./authRole");
 module.exports = {
-    authJwt,
-    verifySignUp
+  authJwt,
+  verifySignUp,
+  authRole,
 };
