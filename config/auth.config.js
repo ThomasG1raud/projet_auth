008da81315c9cdadf128978421bcd256db89971b
ThomasG1raud <91986615+ThomasG1raud@users.noreply.github.com>
2023-04-06 20:06:29 +0200

    scrape

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 module.exports = {
-    // vous pouvez modifier cette valeur
     secret: "darth-vader",
     jwtExpiration: 60,
     jwtRefreshExpiration: 120
---
+++ scrape/books.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const url = "http://books.toscrape.com/catalogue/category/books_1/"
+const book_data = []
+async function getBooks(url){
+    try{
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+
+        const books = $("article");
+        books.each(function (){
+            title = $(this).find("h3 a").text();
+            price = $(this).find(".price_color").text();
+            stock = $(this).find(".availability").text().trim();
+
+            book_data.push({title,price,stock})
+        });
+
+        if($(".next a").length > 0){
+            url = "http://books.toscrape.com/catalogue/category/books_1/"
+            next_page = url + $(".next a").attr("href");
+            getBooks(next_page);
+        }
+        console.log(book_data);
+    }
+    catch (error){
+        console.error(error);
+    }
+}
+
+getBooks(url);
+
+
